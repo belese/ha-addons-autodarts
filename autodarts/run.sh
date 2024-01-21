b8135376e4b2eb841e6034dfa9a21e98bd24c3ce
belese <besme2@gmail.com>
2024-01-21 19:12:39 +0100

    Update run.sh

--- autodarts/run.sh
+++ autodarts/run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/with-contenv bashio
 
-mkdir -p .config
+mkdir -p ~/.config
 
 
 echo "$(ls /config)"
-echo "$(ls .config)"
+echo "$(ls ~/.config)"
 #mount config volume if not exist
-if [ ! -e ".config/autodarts" ]; then
-    ln -s /config .config/autodarts
+if [ ! -e "~/.config/autodarts" ]; then
+    ln -s /config ~/.config/autodarts
 fi
 
 # run autodarts
